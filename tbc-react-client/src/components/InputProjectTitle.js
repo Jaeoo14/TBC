96768a5c4bf5fe790bfe082cbb86f04978f56f01
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-11 15:40:36 +0900

    Update InputProjectTitle.js

--- tbc-react-client/src/components/InputProjectTitle.js
+++ tbc-react-client/src/components/InputProjectTitle.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import CustomInput from './CustomInput';
 import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
@@ -15,12 +15,22 @@ const InputProjectTitle = () => {
 
     return (
         <Container>
+            <h6>프로젝트 제목</h6>
+            <Form.Group>
+                <Form.Text className='text-muted'>프로젝트에 멋진 제목을 붙여주세요. 감정에 호소하는 제목보다는 만드시려는 창작물, 작품명, 혹은 프로젝트의 주제가 드러나게 써주시는 것이 좋습니다.
+            공간이 부족한 곳에 쓰일 7자 이내의 짧은 제목도 정해주세요.</Form.Text>
+            </Form.Group>
             <Row>
                 <Col xs={8}>
-                    <CustomInput title='프로젝트 제목' placeholder='제목을 입력해 주세요.' maxlen='32' handleText={handleProjectTitle} />
+                    <CustomInput
+                        header=''
+                        title='프로젝트 제목'
+                        desc=''
+                        placeholder='제목을 입력해 주세요.'
+                        maxlen='32' handleText={handleProjectTitle} />
                 </Col>
                 <Col>
-                    <CustomInput title='프로젝트 짧은 제목' placeholder='짧은 제목을 입력해 주세요.' maxlen='7' handleText={handleProjectShortTitle} />
+                    <CustomInput header='' title='프로젝트 짧은 제목' desc='' placeholder='짧은 제목을 입력해 주세요.' maxlen='7' handleText={handleProjectShortTitle} />
                 </Col>
             </Row>
             <Row style={{ justifyContent: 'flex-end' }}>
