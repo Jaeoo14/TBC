d4ee9c780b54b325d40798048f8a171327e72e5f
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-11 12:04:03 +0900

    Update App.js

--- tbc-react-client/src/App.js
+++ tbc-react-client/src/App.js
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header2 from './Nav/Header2';
 // import Discover from './Discover/Discover';
 
+// import EditProject from './components/EditProject';
+
 function App() {
   return (
     <div className="App">
       <Header2 />
       {/* 아직 라우터 설정을 안해서 잠시 이렇게 작업하고 있습니다... 밑에 주석 푸시면 보입니다 */}
       {/* <Discover /> */}
+      {/* <EditProject /> */}
     </div>
   );
 }
