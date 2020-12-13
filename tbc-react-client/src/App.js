2d5c266f42c973cd2227d607b8b56edb0fb9f04b
brown2243 <brown2243@naver.com>
2020-12-13 10:50:25 +0900

    router 적용전

--- tbc-react-client/src/App.js
+++ tbc-react-client/src/App.js
@@ -14,8 +14,9 @@ function App() {
       </Container>
       <hr />
       {/* 아직 라우터 설정을 안해서 잠시 이렇게 작업하고 있습니다... 밑에 주석 푸시면 보입니다 */}
-      <UserSetting />
-      {/* <Discover /> */}
+      {/* <UserSetting /> */}
+      <Discover />
+
       {/* <EditProject /> */}
     </div>
   );
