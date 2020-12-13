f13c0b2539cf2ba80f849cc643e257d3ac66b1ef
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-14 08:17:44 +0900

    Update UploadProjectImage.js

--- tbc-react-client/src/components/UploadProjectImage.js
+++ tbc-react-client/src/components/UploadProjectImage.js
@@ -1,26 +1,50 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+
 import { Form, Button, Container, Row } from 'react-bootstrap';
 import CheckIcon from '@material-ui/icons/Check';
-import CloseIcon from '@material-ui/icons/Close';
+// import CloseIcon from '@material-ui/icons/Close';
 
-const UploadProjectImage = () => {
-    const [image, setImage] = useState(null);
+import Pas from '../ProjectApiService';
 
-    const handleSave = () => { }
+export default class UploadProjectImage extends Component {
+	state = {
+		file: undefined,
+		info: undefined,
+	};
 
-    return (
-        <Container>
-            <Row>
-                <Form.Group>
-                    <Form.Label column='sm' style={{ fontWeight: 'bolder', color: 'black' }}>프로젝트 대표 이미지</Form.Label>
-                    <Form.File id="project-image" label="" />
-                </Form.Group>
-            </Row>
-            <Row style={{ justifyContent: 'flex-end' }}>
-                <Button variant='primary' size='sm' onClick={handleSave}><CheckIcon />닫기</Button>
-            </Row>
-        </Container>
-    );
-};
+	componentDidMount() {}
 
-export default UploadProjectImage;
+	componentDidUpdate(prevProps, prevState) {}
+
+	selectFile = e => {
+		this.setState({ file: e.target.files[0] }, () => console.log(this.state));
+	};
+
+	uploadFile = () => {
+		Pas.upload(this.state.file)
+			.then(res => Pas.getFile(res.data))
+			.then(res => this.setState({ info: res.data }, ()=>console.log('UploadProjectImage', this.state)))
+			.catch(err => console.log(err));
+	};
+
+	render() {
+		return (
+			<Container>
+				<Row>
+					<Form.Group>
+						<Form.Label column='sm' style={{ fontWeight: 'bolder', color: 'black' }}>
+							프로젝트 대표 이미지
+						</Form.Label>
+						<Form.File id='project-image' label='' onChange={this.selectFile} formEncType='multipart/form-data' />
+					</Form.Group>
+				</Row>
+				<Row style={{ justifyContent: 'flex-end' }}>
+					<Button variant='primary' size='sm' onClick={this.uploadFile}>
+						<CheckIcon />
+						저장하기
+					</Button>
+				</Row>
+			</Container>
+		);
+	}
+}
