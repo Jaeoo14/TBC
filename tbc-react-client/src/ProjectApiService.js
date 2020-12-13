0e109d8c1e743ae789d8a09149bdd7ad4c265623
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-14 08:17:39 +0900

    Update ProjectApiService.js

--- tbc-react-client/src/ProjectApiService.js
+++ tbc-react-client/src/ProjectApiService.js
@@ -1,4 +1,5 @@
 import Axios from 'axios';
+import Http from './HttpCommon';
 import { Component } from 'react';
 
 // front-end : port 3000
@@ -8,32 +9,49 @@ import { Component } from 'react';
 const URL = 'http://localhost:8090/project'; // https는 따로 알아보자.
 
 class ProjectApiService extends Component {
-  // http://localhost:8090/project
-  // 전체 프로젝트들 가져오기
-  fetch() {
-    return Axios.get(URL);
-  }
+	// http://localhost:8090/project
+	// 전체 프로젝트들 가져오기
+	fetch() {
+		return Axios.get(URL);
+	}
 
-  // http://localhost:8090/project/1
-  // id인 프로젝트 가져오기
-  fetchBy(id) {
-    return Axios.get(URL + '/' + id);
-  }
+	// http://localhost:8090/project/1
+	// id인 프로젝트 가져오기
+	fetchBy(id) {
+		return Axios.get(URL + '/' + id);
+	}
 
-  // 프로젝트 추가
-  insert(project) {
-    return Axios.post(URL, project);
-  }
-  
-  // 프로젝트 갱신
-  update(project) {
-    return Axios.put(URL + '/' + project.id, project);
-  }
+	// 프로젝트 추가
+	insert(project) {
+		return Axios.post(URL, project);
+	}
 
-  // id인 프로젝트 삭제.
-  delete(id) {
-    return Axios.delete(URL + '/' + id);
-  }
+	// 프로젝트 갱신
+	update(project) {
+		return Axios.put(URL + '/' + project.id, project);
+	}
+
+	// id인 프로젝트 삭제.
+	delete(id) {
+		return Axios.delete(URL + '/' + id);
+	}
+
+  // upload project image file.
+	upload(file) {
+		console.log('upload...', file);
+		let formData = new FormData();
+		formData.append('file', file);
+		console.log('formData', formData);
+		return Http.post('', formData, {
+			headers: { 'Content-Type': 'multipart/form-data' },
+		});
+	}
+
+  // download project image file.
+	getFile(fileId) {
+		console.log('getFile...', fileId);
+		return Http.get('/' + fileId);
+	}
 }
 
 export default new ProjectApiService();
