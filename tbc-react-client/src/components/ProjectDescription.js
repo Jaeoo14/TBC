ee426c6c0da0f820866f5321e357dc25679699ad
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-14 19:04:05 +0900

    Update ProjectDescription.js

--- tbc-react-client/src/components/ProjectDescription.js
+++ tbc-react-client/src/components/ProjectDescription.js
@@ -21,13 +21,9 @@ import InputTags from './InputTags';
 import Pas from '../ProjectApiService';
 
 class ProjectDescription extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			project: {},
-		};
-	}
+	state = {
+		project: {},
+	};
 
 	componentDidMount() {
 		const { pId, cId } = this.props;
