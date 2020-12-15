a994b1014629b4397dd56f551c3f73ad96588e26
JSE <sengnim321@gmail.com>
2020-12-15 09:00:31 +0900

    헤더에 로고 추가

--- tbc-react-client/src/Nav/Header2.js
+++ tbc-react-client/src/Nav/Header2.js
@@ -15,11 +15,6 @@ const Header2 = () => {
         <li>
           <a href="#">프로젝트 올리기</a>
         </li>
-        <img
-          className="logo"
-          src="https://www.flaticon.com/svg/static/icons/svg/2809/2809124.svg"
-          alt="logo"
-        />
         <li className="login">
           <a href="/login"> 로그인/회원가입</a>
         </li>
@@ -28,6 +23,13 @@ const Header2 = () => {
             <SearchIcon />
           </a>
         </li>
+      
+        <img
+          className="logo"
+          src="logo.png"
+          alt="logo"
+        />
+        
       </ul>
     </div>
   );
