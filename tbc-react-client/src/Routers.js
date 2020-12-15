771f3aba5c69a9b240a3a4406cf47e34885e4c37
JSE <sengnim321@gmail.com>
2020-12-15 16:34:20 +0900

    오류 수정

--- tbc-react-client/src/Routers.js
+++ tbc-react-client/src/Routers.js
@@ -5,7 +5,6 @@ import Login from "./member/Login";
 import UserSetting from "./member/UserSetting";
 import EditProject from "./components/EditProject";
 import Discover from "./Discover/Discover";
-import Card from "./Main/Card";
 
 class Routers extends React.Component {
   constructor(props) {
@@ -20,7 +19,6 @@ class Routers extends React.Component {
           <Route path="/EditProject" component={EditProject} />
           <Route path="/discover" component={Discover} />
           <Route path="/Discover" component={Discover} />
-          <Route path="/card" component={Card} />
           
 
         </Switch>
