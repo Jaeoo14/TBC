7800eea578a1922ab3815454b3309462d66fdc51
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-16 23:59:42 +0900

    프로젝트 올리기 페이지에서 빈 프로젝트를 만든다.
    프로젝트 테이블에 insert sql문을 변경.
    새 프로젝트를 만들 때 소유자 이름만 추가하고 나머지 컬럼은 테이블에 설정된 기본값으로 세팅된다.
    문자열은 '', 숫자는 0, 날짜는 프로젝트를 만든 날짜로 설정된다.

--- tbc-react-client/src/components/Start.js
+++ tbc-react-client/src/components/Start.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import { Jumbotron, Container, Button } from 'react-bootstrap';
+import Pas from '../ProjectApiService';
 
 import './Start.css';
 
 class Start extends Component {
+	// login 한 사용자만 프로젝트를 만들 수 있다.
+	// login 한 사용자의 id를 알아야 한다.
+	state = {
+		id: undefined,
+		creatorId: 999, // 임시로 cId=999로 설정한다.
+	};
+
+	componentDidMount() {}
+
+	makeNewProject = () => {
+		Pas.insert(this.state) // 빈 프로젝트를 DB에 추가
+			.then(res => this.setState({ id: res.data }, this.gotoEditProject))
+			.catch(err => console.log(err));
+	};
+
+	gotoEditProject = () => {
+		console.log('New Project', this.state);
+
+		this.props.history.push({
+			pathname: '/editproject',
+			state: this.state,
+		});
+	};
+
 	render() {
 		return (
 			<Jumbotron fluid>
 				<Container>
-					<h1 className='shadowText' style={{ color: 'white' }}>마음 속 프로젝트 아이디어,<br/>텀블벅에서 현실로.</h1>
-					<h6 className='shadowText' style={{ color: 'white' }}>크라우드펀딩으로 프로젝트를 위한 자금도 모으고, 든든한 후원자 네트워크도 확보할 수 있습니다.</h6>
+					<h1 className='shadowText' style={{ color: 'white' }}>
+						마음 속 프로젝트 아이디어,
+						<br />
+						텀블벅에서 현실로.
+					</h1>
+					<h6 className='shadowText' style={{ color: 'white' }}>
+						크라우드펀딩으로 프로젝트를 위한 자금도 모으고, 든든한 후원자 네트워크도 확보할 수 있습니다.
+					</h6>
 					<p>
-						<Button variant='warning' onClick={()=>this.props.history.push('/editproject')}>지금 시작하기</Button>
+						<Button variant='warning' onClick={this.makeNewProject}>
+							<strong>지금 시작하기</strong>
+						</Button>
 					</p>
 				</Container>
 			</Jumbotron>
