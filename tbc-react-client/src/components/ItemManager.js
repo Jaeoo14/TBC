b793acdad1e2146c7ef514f459de7a62b54bb8aa
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-16 11:51:10 +0900

    item table 추가, 삭제

--- tbc-react-client/src/components/ItemManager.js
+++ tbc-react-client/src/components/ItemManager.js
@@ -1,18 +1,106 @@
 import React, { Component } from 'react';
-import { Button, Container, Modal } from 'react-bootstrap';
+import { Button, Container, ListGroup, Modal, Row, Table } from 'react-bootstrap';
+
+import Pas from '../ProjectApiService';
 
 import CheckIcon from '@material-ui/icons/Check';
-import CloseIcon from '@material-ui/icons/Close';
+// import CloseIcon from '@material-ui/icons/Close';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+// import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 
-import './ItemManager.css'
+import MakeItem from './MakeItem';
+
+import './ItemManager.css';
 
 class ItemManager extends Component {
 	state = {
 		show: false,
+		items: [],
+		newItem: {},
+		temp1: '',
+		temp2: '',
+		showMakeItem: false,
 	};
 
-	handleClose = () => this.setState({ show: false });
-	handleShow = () => this.setState({ show: true });
+	readItemList = () => {
+		Pas.getItems()
+			.then(res => {
+				this.setState({ items: res.data }, () => console.log('ItemManager.readItemList items=', this.state.items));
+			})
+			.catch(err => console.log(err));
+	};
+
+	handleClose = () => {
+		console.log('ImageManager:handleClose', this.state);
+		this.setState({ show: false });
+	};
+
+	handleShow = () => {
+		console.log('ImageManager:handleShow', this.state);
+		this.setState({ show: true });
+		this.readItemList();
+	};
+
+	handleAddItem = () => {
+		this.setState({showMakeItem : true});
+	};
+
+	handleMakeItemClose = () => {
+		this.setState({showMakeItem : false});
+		this.readItemList();
+	}
+
+	componentDidMount() {
+		// console.log('ImageManager:componentDidMount', this.state);
+		// if (this.state.show)
+		// 	this.readImageList();
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		// console.log('ImageManager:componentDidUpdate', this.state, prevState);
+		// if (this.state.show && this.state.items !== prevState.items)
+		// 	this.readImageList();
+	}
+
+	handleTemp1 = text => this.setState({ temp1: text });
+	handleTemp2 = text => this.setState({ temp2: text });
+
+	handleNewItem = (name, value) => {
+		this.setState({ newItem: { ...this.state.newItem, [name]: value } }, () => console.log(this.state.newItem));
+	};
+
+	handleItemName = name => {
+		this.handleNewItem('name', name);
+	};
+
+	handleItemOption = e => {
+		this.handleNewItem('opt', e.target.value);
+	};
+
+	handleDeleteItem = id =>
+		Pas.deleteItem(id)
+			.then(res => Pas.getItems())
+			.then(res => this.setState({ items: res.data, newItem: {}, temp1: '', temp2: '' }, () => console.log(this.state)))
+			.catch(err => console.log(err));
+
+	handleUpdateItem = item =>
+		Pas.putItem(item)
+			.then(res => Pas.getItems())
+			.then(res => this.setState({ items: res.data, newItem: {}, temp1: '', temp2: '' }, () => console.log(this.state)))
+			.catch(err => console.log(err));
+
+	handleSave = () => {
+		let msg = '';
+		if (this.state.newItem.opt === '1') msg = this.state.temp1;
+		else if (this.state.newItem.opt === '2') msg = this.state.temp2;
+
+		this.handleNewItem('message', msg);
+
+		Pas.postItem(this.state.newItem)
+			.then(res => Pas.getItems())
+			.then(res => this.setState({ items: res.data, newItem: {}, temp1: '', temp2: '' }, () => console.log(this.state)))
+			.catch(err => console.log(err));
+	};
 
 	render() {
 		return (
@@ -26,12 +114,65 @@ class ItemManager extends Component {
 						<Modal.Title>아이템 관리하기</Modal.Title>
 					</Modal.Header>
 					<Modal.Body>
-          </Modal.Body>
+						{!this.state.showMakeItem && <ListGroup id='addItem-1'>
+							<ListGroup.Item as='div' action>
+								<Row>
+									<strong>아이템 만들기</strong>
+									<sub>선물 구성에 추가할 아이템을 만듭니다.</sub>
+								</Row>
+								<Row className='mt-1'>
+									<Button variant='outline-danger' size='sm' onClick={this.handleAddItem}>
+										<AddCircleOutlineIcon />
+										아이템 추가하기
+									</Button>
+								</Row>
+							</ListGroup.Item>
+						</ListGroup>}
+						{this.state.showMakeItem && <ListGroup id='addItem-2'>
+							<ListGroup.Item as='div' action>
+								<MakeItem handleClose={this.handleMakeItemClose} />
+							</ListGroup.Item>
+						</ListGroup>}
+						<hr></hr>
+						<div>아이템 목록</div>
+						<Table hover responsive size='sm'>
+							<thead style={{ backgroundColor: '#eee' }}>
+								<tr>
+									<th>ID</th>
+									<th>이름</th>
+									<th>옵션</th>
+									<th></th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.items.map(item => (
+									<tr key={item.id}>
+										<td>{item.id}</td>
+										<td>{item.name}</td>
+										<td>{item.opt}</td>
+										<td style={{ textAlign: 'left' }}>{item.message}</td>
+										<td style={{ textAlign: 'right' }}>
+											<Button variant='outline-primary mr-1' size='sm' onClick={() => this.handleUpdateItem(item)}>
+												수정하기
+											</Button>
+											<Button variant='outline-danger' size='sm' onClick={() => this.handleDeleteItem(item.id)}>
+												삭제하기
+											</Button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</Table>
+					</Modal.Body>
 					<Modal.Footer>
-						<Button variant='primary' size='sm' onClick={this.handleClose}><CheckIcon/>닫기</Button>
+						<Button variant='primary' size='sm' onClick={this.handleClose}>
+							<CheckIcon />
+							닫기
+						</Button>
 					</Modal.Footer>
 				</Modal>
-      </Container>
+			</Container>
 		);
 	}
 }
---
+++ tbc-react-client/src/components/MakeItem.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import { Button, Form, Row } from 'react-bootstrap';
+import CustomInput from './CustomInput';
+import CustomTextArea from './CustomTextArea';
+
+import Pas from '../ProjectApiService';
+
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+
+class MakeItem extends Component {
+	state = {
+    name: '',
+    opt: '0',
+		opt1Msg: '',
+    opt2Msg: '',
+  };
+
+  handleClose = () => {
+    this.props.handleClose();
+  }
+
+	handleSave = () => {
+    let item = {
+      name: this.state.name,
+      opt: this.state.opt,
+      message: '',
+    }
+
+		if (this.state.opt === '1') item.message = this.state.opt1Msg;
+		else if (this.state.opt === '2') item.message = this.state.opt2Msg;
+	
+    Pas.postItem(item)
+      .then(res=>{console.log('item=', item); this.handleClose();})
+      .catch(err => console.log(err));
+	};
+
+  handleName = text => this.setState({ name: text }, console.log(this.state));
+  handleOpt = e => this.setState({ opt: e.target.value }, console.log(this.state));
+	handleOpt1Msg = text => this.setState({ opt1Msg: text }, console.log(this.state));
+  handleOpt2Msg = text => this.setState({ opt2Msg: text }, console.log(this.state));
+  
+  // handleItem = (name, value) => {
+  //   this.setState({[name]:value});
+  // }
+
+	render() {
+		return (
+			<Form>
+				<Form.Group controlId='make-item'>
+					<Form.Label>아이템 만들기</Form.Label>
+					<CustomInput
+						header=''
+						width='50%'
+						title='아이템 이름'
+						desc=''
+						placeholder='새로 만들 아이템의 이름을 입력해주세요.'
+						minlen='0'
+						maxlen='50'
+						value={this.state.name}
+						handleText={this.handleName}
+					/>
+				</Form.Group>
+				<fieldset>
+					<Form.Group controlId='item-option'>
+						<Form.Label>옵션조건</Form.Label>
+            <Form.Check 
+              type='radio' 
+              name='option-type' 
+              value='0' 
+              label='옵션이 필요 없는 아이템입니다.' 
+              onChange={this.handleOpt} />
+						<Form.Check
+							type='radio'
+							name='option-type'
+							value='1'
+							label='주관식 옵션이 필요한 아이템입니다. (각인, 메시지 등)'
+							onChange={this.handleOpt}
+						/>
+						{this.state.opt === '1' && (
+							<CustomTextArea
+								placeholder='후원자에게 안내할 메시지를 작성해 주세요.
+            예시) 사이즈, 메시지 순으로 작성해 주세요.'
+								maxlen='100'
+								handleText={this.handleOpt1Msg}
+								noButtons={true}
+								value={this.state.opt1Msg}
+							/>
+						)}
+						<Form.Check
+							type='radio'
+							name='option-type'
+							value='2'
+							label='객관식 옵션이 필요한 아이템입니다. (사이즈, 색상 등)'
+							onChange={this.handleOpt}
+						/>
+						{this.state.opt === '2' && (
+							<CustomTextArea
+								placeholder='옵션 항목을 입력해 주세요.
+            옵션 항목은 줄바꿈으로 구분됩니다.
+            예시) 블랙 - 230mm
+            화이트 - 240mm'
+								maxlen='500'
+								handleText={this.handleOpt2Msg}
+								rows={5}
+								noButtons={true}
+								value={this.state.opt2Msg}
+							/>
+						)}
+					</Form.Group>
+				</fieldset>
+				<Row style={{ justifyContent: 'flex-end' }}>
+					<Button variant='secondary mr-1' size='sm' onClick={this.handleClose}>
+						<CloseIcon fontSize='small' />취소하기</Button>
+					<Button variant='primary' size='sm' onClick={this.handleSave}>
+						<CheckIcon fontSize='small' />저장하기</Button>
+				</Row>
+			</Form>
+		);
+	}
+}
+
+export default MakeItem;
