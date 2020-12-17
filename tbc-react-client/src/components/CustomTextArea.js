5ab2959a6f05292991bbba8e47bbed70e3cdb9a4
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-17 16:25:40 +0900

    Update CustomTextArea.js

--- tbc-react-client/src/components/CustomTextArea.js
+++ tbc-react-client/src/components/CustomTextArea.js
@@ -62,14 +62,10 @@ class CustomTextArea extends Component {
 				</Row>
 				{ !this.props.noButtons && 
 				<Row style={{ justifyContent: 'flex-end' }}>
-					<Button variant='secondary mr-1' size='sm'>
-						<CloseIcon />
-						취소하기
-					</Button>
+					<Button variant='secondary mr-1' size='sm' onClick={this.props.handleClose}>
+						<CloseIcon fontSize='small'/>취소하기</Button>
 					<Button variant='primary' size='sm' disabled={text.length < minlen} onClick={this.handleSave}>
-						<CheckIcon />
-						저장하기
-					</Button>
+						<CheckIcon fontSize='small'/>저장하기</Button>
 				</Row>}
 			</Container>
 		);
