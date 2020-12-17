b417066100473cf4589d1de037c12ecf1f44bbe1
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-17 15:16:29 +0900

    key props 추가

--- tbc-react-client/src/Main/Cards.js
+++ tbc-react-client/src/Main/Cards.js
@@ -1,146 +1,139 @@
 import { Button } from '@material-ui/core';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {  Card, CardDeck } from 'react-bootstrap';
+import { Card, CardDeck } from 'react-bootstrap';
 import './pagination.css';
 import './Card.css';
-import jquery from 'jquery'
+import jquery from 'jquery';
 import ProjectApiService from '../ProjectApiService';
 import Carousel from 'react-elastic-carousel';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import NotFavoriteIcon from '@material-ui/icons/FavoriteBorderOutlined';
 //import DisplayImage from '../components/DisplayImage';
 
 const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 }
-  ];
+	{ width: 1, itemsToShow: 1 },
+	{ width: 550, itemsToShow: 2, itemsToScroll: 2 },
+	{ width: 768, itemsToShow: 3 },
+	{ width: 1200, itemsToShow: 4 },
+];
 
 class Cards extends Component {
-    
-        componentDidMount() {
-            window.$ = window.jQuery = jquery;
-            this.viewProjectList();
-            // this.viewRemainDate();
-            this.viewCountProject();
-        }
-    
-        constructor(props) {
-            super(props);
+	componentDidMount() {
+		window.$ = window.jQuery = jquery;
+		this.viewProjectList();
+		// this.viewRemainDate();
+		this.viewCountProject();
+	}
 
-            this.state = {
-                lists: [],
-                count: 0,
-            };
-        };
-        viewCountProject = () => {
+	constructor(props) {
+		super(props);
 
-            ProjectApiService.countProject()
-            .then(res => {
-                this.setState({ count : res.data });
-                console.log('viewCountProject의 값', res.data)
-            })
-            .catch(err => {
-                console.error('Discover.js의 viewCountProject() 에러!', err);
-            })
-        }
-        viewProjectList = () => {
+		this.state = {
+			lists: [],
+			count: 0,
+		};
+	}
+	viewCountProject = () => {
+		ProjectApiService.countProject()
+			.then(res => {
+				this.setState({ count: res.data });
+				console.log('viewCountProject의 값', res.data);
+			})
+			.catch(err => {
+				console.error('Discover.js의 viewCountProject() 에러!', err);
+			});
+	};
+	viewProjectList = () => {
+		ProjectApiService.projectList()
+			.then(res => {
+				this.setState({ lists: res.data });
+				console.log('projectList 값', res.data);
+			})
+			.catch(err => {
+				console.error('Discover.js의 viewProjectList() 에러!', err);
+			});
+	};
+	toggleLike = id => {
+		console.log(`id = > ${id}`);
+		const localLiked = !this.state.liked;
+		this.setState({ liked: localLiked });
+	};
+	render() {
+		return (
+			<div style={{ display: 'inline' }}>
+				<div style={{ width: 1500, display: 'inline-flex', verticalAlign: 'center' }}>
+					{/* <h4 style={{textAlign:"center"}}>주목할 만한 프로젝트</h4> */}
+					<Carousel breakPoints={breakPoints}>
+						{this.state.lists.map(list => (
+							<div className='Card' key={list.id}>
+								<CardDeck>
+									{/* //     좋아요기능      */}
+									<Card style={{ width: 240 }}>
+										<div style={{ float: 'right' }} onClick={() => this.toggleLike()}>
+											{this.state.liked === false ? <NotFavoriteIcon /> : <FavoriteIcon color='secondary' />}
+										</div>
+										<Card.Img variant='top' src={list.mainImg} style={{ height: 180 }} />
+										{/* <Card.Img><DisplayImage pId={this.props.id} width="280px" height="240px" /></Card.Img> */}
+										<Card.Body>
+											<Card.Subtitle style={{ fontSize: 10, textAlign: 'left' }}>
+												{list.category} | {list.creatorId}
+											</Card.Subtitle>
+											<Card.Title>{list.shortTitle}</Card.Title>
+											<Card.Text style={{ fontSize: 15 }}>{list.content}</Card.Text>
+										</Card.Body>
+										<Card.Footer>
+											<small className='text-muted'>
+												<span style={{ color: '#ff4646', fontSize: 15 }}> nnn%</span>
+											</small>
+										</Card.Footer>
+									</Card>
+								</CardDeck>
+							</div>
+						))}
+					</Carousel>
+				</div>
 
-            ProjectApiService.projectList()
-            .then(res => {
-                this.setState({ lists : res.data });
-                console.log('projectList 값', res.data)
-    
-            })
-            .catch(err => {
-                console.error('Discover.js의 viewProjectList() 에러!', err);
-            })
-        }
-        toggleLike = (id) => {
-            console.log(`id = > ${id}`);
-            const localLiked = !this.state.liked;        
-            this.setState({ liked : localLiked });
-        };
-            render() {
-            return (
-            <div style={{display: 'inline'}}>
-            <div style={{width:1500, display: 'inline-flex', verticalAlign: 'center'}}>
-                 {/* <h4 style={{textAlign:"center"}}>주목할 만한 프로젝트</h4> */}
-                 <Carousel breakPoints={breakPoints}>
-                   {this.state.lists.map(list => 
-                <div className='Card' >
-                    <CardDeck  >
-                            {/* //     좋아요기능      */}
-                            <Card style={{width: 240}}>
-                                 <div style={{float:"right"}} onClick={() => this.toggleLike()}>
-                                  {this.state.liked === false ? <NotFavoriteIcon /> : <FavoriteIcon color="secondary" />} 
-                              </div>
-                            <Card.Img variant="top" src={list.mainImg} style={{ height: 180}} />
-                            {/* <Card.Img><DisplayImage pId={this.props.id} width="280px" height="240px" /></Card.Img> */}
-                            <Card.Body>
-                              <Card.Subtitle style={{fontSize: 10, textAlign: 'left'}}>
-                                    {list.category} | {list.creatorId}
-                              </Card.Subtitle>
-                              <Card.Title>{list.shortTitle}</Card.Title>
-                              <Card.Text style={{fontSize: 15}}>
-                                    {list.content}
-                              </Card.Text>
-                            </Card.Body>
-                            <Card.Footer>
-                              <small className="text-muted"><span style={{color:"#ff4646", fontSize:15}}> nnn%</span></small>
-                            </Card.Footer>
-                          </Card>
-                    </CardDeck>
-                </div>
-                    )}
-                </Carousel>
-                    
-                    </div>
-                    
-            <div style={{width:1500, display: 'inline-flex', verticalAlign: 'center'}}>
-                 {/* <h4 style={{textAlign:"center"}}>주목할 만한 프로젝트</h4> */}
-                 <Carousel breakPoints={breakPoints}>
-                   {this.state.lists.map(list => 
-                     <div className='Card' >
-                      <CardDeck  >
-                            {/* //     좋아요기능      */}
-                            <Card style={{width: 240}}>
-                                 <div style={{float:"right"}} onClick={() => this.toggleLike()}>
-                                  {this.state.liked === false ? <NotFavoriteIcon /> : <FavoriteIcon color="secondary" />} 
-                              </div>
-                            <Card.Img variant="top" src={list.mainImg} style={{ height: 180}} />
-                            {/* <Card.Img><DisplayImage pId={this.props.id} width="280px" height="240px" /></Card.Img> */}
-                            <Card.Body>
-                              <Card.Subtitle style={{fontSize: 10, textAlign: 'left'}}>
-                                    {list.category} | {list.creatorId}
-                              </Card.Subtitle>
-                              <Card.Title>{list.shortTitle}</Card.Title>
-                              <Card.Text style={{fontSize: 15}}>
-                                    {list.content}
-                              </Card.Text>
-                            </Card.Body>
-                            <Card.Footer>
-                              <small className="text-muted"><span style={{color:"#ff4646", fontSize:15}}> nnn%</span></small>
-                            </Card.Footer>
-                          </Card>
-                      </CardDeck>
-                     </div>
-                     )}
-                 </Carousel>
-                    
-                    </div>
-                    {/* <div>
+				<div style={{ width: 1500, display: 'inline-flex', verticalAlign: 'center' }}>
+					{/* <h4 style={{textAlign:"center"}}>주목할 만한 프로젝트</h4> */}
+					<Carousel breakPoints={breakPoints}>
+						{this.state.lists.map(list => (
+							<div className='Card' key={list.id}>
+								<CardDeck>
+									{/* //     좋아요기능      */}
+									<Card style={{ width: 240 }}>
+										<div style={{ float: 'right' }} onClick={() => this.toggleLike()}>
+											{this.state.liked === false ? <NotFavoriteIcon /> : <FavoriteIcon color='secondary' />}
+										</div>
+										<Card.Img variant='top' src={list.mainImg} style={{ height: 180 }} />
+										{/* <Card.Img><DisplayImage pId={this.props.id} width="280px" height="240px" /></Card.Img> */}
+										<Card.Body>
+											<Card.Subtitle style={{ fontSize: 10, textAlign: 'left' }}>
+												{list.category} | {list.creatorId}
+											</Card.Subtitle>
+											<Card.Title>{list.shortTitle}</Card.Title>
+											<Card.Text style={{ fontSize: 15 }}>{list.content}</Card.Text>
+										</Card.Body>
+										<Card.Footer>
+											<small className='text-muted'>
+												<span style={{ color: '#ff4646', fontSize: 15 }}> nnn%</span>
+											</small>
+										</Card.Footer>
+									</Card>
+								</CardDeck>
+							</div>
+						))}
+					</Carousel>
+				</div>
+				{/* <div>
                         <Button style={{backgroundColor: 'whitesmoke', borderRadius: 20, fontSize: 16, paddingLeft: 30, paddingRight: 30}} href="#">???? 프로젝트 더보기</Button>
                     </div> */}
-                    </div>
-        );
-    }
+			</div>
+		);
+	}
 }
 
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<Cards />, rootElement);
 export default Cards;
