b7fb04aa695fafa8ffdea9f2f6040ca26d70dbe3
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-19 23:54:06 +0900

    ProfileImage 추가
    사용자 id 와 크기를 받아서 해당 사용자의 프로필 이미지를 그린다.

---
+++ tbc-react-client/src/member/components/ProfileImage.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import { Image } from 'react-bootstrap';
+import Pas from '../../ProjectApiService';
+
+const ProfileImage = (props) => {
+  const {userId, width, height} = props;
+
+  const [image, setImage] = useState(undefined);
+  
+  useEffect(()=>{
+    Pas.getUser(userId)
+      .then(res=>Pas.getFile(res.data.profileImg))
+      .then(res=>setImage(res.data))
+      .catch(err=>console.log('Can not read profile image from DB !', err));
+  },[userId]);
+
+  return (image !== undefined && 
+      		<Image src={`data:image/png;base64,${image.data}`} alt='profile image' 
+			      style={{width:width, height:height}} 
+			      roundedCircle />
+  );
+};
+
+ProfileImage.defaultProps={
+  width:'50px',
+  height:'50px',
+}
+
+export default ProfileImage;
+
