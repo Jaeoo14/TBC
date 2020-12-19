6ceff2804df9ddbc455f0006dec99ae0e2389a6c
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-19 21:12:39 +0900

    Update DetailMapper.java

--- tbc-spring-server/src/main/java/com/team2/tbcserver/mapper/DetailMapper.java
+++ tbc-spring-server/src/main/java/com/team2/tbcserver/mapper/DetailMapper.java
@@ -2,7 +2,6 @@ package com.team2.tbcserver.mapper;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.team2.tbcserver.vo.DetailVO;
-import com.team2.tbcserver.vo.ProjectVO;
 
 @Mapper
 public interface DetailMapper {
