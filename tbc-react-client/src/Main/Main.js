1aa412017c46ae5790d642ed40035a795f535e25
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-20 23:40:19 +0900

    메인 슬라이드 그리기

--- tbc-react-client/src/Main/Main.js
+++ tbc-react-client/src/Main/Main.js
@@ -1,65 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import { Carousel } from 'react-bootstrap';
 import ProjectApiService from '../ProjectApiService';
 import Cards from './Cards';
 import MainItem from './MainItem';
+
 class Main extends Component {
-  componentDidMount() {
-    this.viewProjectList();
+	state = {
+		lists: [],
+	};
+
+	componentDidMount() {
+		this.viewProjectList();
+	}
+
+	viewProjectList = () => {
+		ProjectApiService.projectList()
+			.then(res => {
+				this.setState({ lists: res.data }, console.log('projectList 값', res.data));
+			})
+			.catch(console.log);
+	};
+
+	// {/* <Carousel.Item>
+	//   <a href="#">
+	//   <img
+	//     className="d-block w-100"
+	//     src="http://ipsumimage.appspot.com/1275x400?text=First slide&bg=373940"
+	//     alt="First slide"
+	//     style={{height:400, width: 1500}}
+	//   />
+	//   </a>
+	//   <Carousel.Caption>
+	//     <h3>First slide label</h3>
+	//     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+	//   </Carousel.Caption>
+	// </Carousel.Item> */}
+
+	render() {
+		if (this.state.lists.length === 0) return null;
+		return (
+			<div style={{ display: 'inline' }}>
+				<div style={{ display: 'inline-flex' }}>
+					<Carousel style={{ width: '1275px' }}>
+						{this.state.lists.map(list => (
+							<Carousel.Item>
+								<MainItem key={list.id} pId={list.id} content={list.content} shortTitle={list.shortTitle} longTitle={list.longTitle} />
+							</Carousel.Item>
+						))}
+					</Carousel>
+				</div>
+				<Cards />
+			</div>
+		);
+	}
 }
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        lists: [],
-        project: this.props.project,
-    };
-};
-viewProjectList = () => {
-
-  ProjectApiService.projectList()
-  .then(res => {
-      this.setState({ lists : res.data });
-      console.log('projectList 값', res.data)
-
-  })
-  .catch(err => {
-      console.error('Discover.js의 viewProjectList() 에러!', err);
-  })
-}
-
-    render() {
-        return (
-          <div style={{display:'inline'}}>
-          <div style={{display:'inline-flex'}}>
-            
-            <Carousel style={{width: '1275px'}}>
-            {this.state.lists.map(list => 
-              // {/* <Carousel.Item>
-              //   <a href="#">
-              //   <img
-              //     className="d-block w-100"
-              //     src="http://ipsumimage.appspot.com/1275x400?text=First slide&bg=373940"
-              //     alt="First slide"
-              //     style={{height:400, width: 1500}}
-              //   />
-              //   </a>
-              //   <Carousel.Caption>
-              //     <h3>First slide label</h3>
-              //     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              //   </Carousel.Caption>
-              // </Carousel.Item> */}
-              <MainItem project={list} />
-            )}  
-            </Carousel>
-            
-        </div>
-            <Cards />
-        </div>
-        );
-    }
-}
-
 
 export default Main;
--- tbc-react-client/src/Main/MainItem.js
+++ tbc-react-client/src/Main/MainItem.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
-import DisplayImage from '../components/DisplayImage';
 
-class MainItem extends Component{
-    
-    render() {
-        return (
-            <div>
-                 <Carousel.Item>
-                <a href="#">
-                {/* <img
-                  className="d-block w-100"
-                  src="http://ipsumimage.appspot.com/1275x400?text=First slide&bg=373940"
-                  alt="First slide"
-                  style={{height:400, width: 1500}}
-                /> */}
-                <DisplayImage pId={this.props.project.id} width="1275px" height="400px" >
-                <Carousel.Caption>
-                  <h3>{this.props.project.shortTitle}</h3>
-                  <p>{this.props.project.content}</p>
-                </Carousel.Caption>
-                </DisplayImage>
-                </a>
-                
-              </Carousel.Item>
-            </div>
-        )
-    }
+import Pas from '../ProjectApiService';
+
+class MainItem extends Component {
+	state = {
+		file: {}, // 이미지 파일 데이타. file 테이블 참조.
+	};
+
+	componentDidMount = () => {
+		Pas.getFileOfProject(this.props.pId)
+			.then(res => this.setState({ file: res.data }, () => console.log(this.state.file)))
+			.catch(err => console.log(err));
+	};
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.pId === this.props.pId) return;
+
+		Pas.getFileOfProject(this.props.pId)
+			.then(res => this.setState({ file: res.data }, () => console.log(this.state.file)))
+			.catch(err => console.log(err));
+	}
+
+	render() {
+		return (
+			<>
+				<img
+					className='d-block w-100'
+					src={`data:${this.state.file.type};base64,${this.state.file.data}`}
+					alt={this.props.shortTitle}
+					style={{ height: 400, width: 1500 }}
+				/>
+				<Carousel.Caption>
+					<h3 style={{ backgroundColor: 'black', color: 'yellow' }}>{this.props.longTitle}</h3>
+					<p style={{ backgroundColor: 'black', color: 'yellow' }}>{this.props.content}</p>
+				</Carousel.Caption>
+			</>
+		);
+	}
 }
 
 export default MainItem;
