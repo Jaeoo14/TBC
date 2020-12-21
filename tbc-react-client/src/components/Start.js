b97ddb4a16e14fe3fe862e6c231ab2537f22e980
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-21 11:25:33 +0900

    로그인 사용자만 프로젝트를 만들 수 있다.

--- tbc-react-client/src/components/Start.js
+++ tbc-react-client/src/components/Start.js
@@ -7,20 +7,26 @@ import './Start.css';
 class Start extends Component {
 	// login 한 사용자만 프로젝트를 만들 수 있다.
 	state = {
-		id: undefined,	// project.id 실제 빈 프로젝트를 DB에 저장한 후 알 수 있는 값이다.
-		creatorId: 999, // login 한 사용자의 id. 임시로 999로 설정한다.
+		projectId: undefined, // 실제 빈 프로젝트를 DB에 저장한 후 알 수 있는 값이다.
+		userId: 0, // login 한 사용자의 id.
 	};
 
-	componentDidMount() {}
+	componentDidMount() {
+		let _userId = 0;
+		const member = JSON.parse(localStorage.getItem('myStorage'));
+		if (member !== null) _userId = member.id;
+
+		this.setState({ userId: _userId });
+	}
 
 	makeNewProject = () => {
 		Pas.insert(this.state) // 빈 프로젝트를 DB에 추가
-			.then(res => this.setState({ id: res.data }, this.gotoEditProject))
+			.then(res => this.setState({ projectId: res.data }, this.gotoEditProject))
 			.catch(err => console.log(err));
 	};
 
 	gotoEditProject = () => {
-		console.log('New Project', this.state);
+		console.log('New Project (id, user)=', this.state);
 
 		this.props.history.push({
 			pathname: '/editproject',
@@ -29,19 +35,20 @@ class Start extends Component {
 	};
 
 	render() {
+		console.log('Start.render state=', this.state);
 		return (
 			<Jumbotron fluid>
-				<Container>
-					<h1 className='shadowText' style={{ color: 'white' }}>
+				<Container style={{ textAlign:'left', color: 'white' }}>
+					<h1 className='shadowText' >
 						마음 속 프로젝트 아이디어,
 						<br />
 						텀블벅에서 현실로.
 					</h1>
-					<h6 className='shadowText' style={{ color: 'white' }}>
+					<h6 className='shadowText'>
 						크라우드펀딩으로 프로젝트를 위한 자금도 모으고, 든든한 후원자 네트워크도 확보할 수 있습니다.
 					</h6>
 					<p>
-						<Button variant='warning' onClick={this.makeNewProject}>
+						<Button variant='warning' onClick={this.makeNewProject} disabled={this.state.userId === 0}>
 							<strong>지금 시작하기</strong>
 						</Button>
 					</p>
