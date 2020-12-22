846f88b3a8b074d63d0482f2d6933eddb6bc6aaf
jeongchaeuk <jeongchaeuk@gmail.com>
2020-12-22 12:03:27 +0900

    Update Routers.js

--- tbc-react-client/src/Routers.js
+++ tbc-react-client/src/Routers.js
@@ -21,7 +21,7 @@ class Routers extends React.Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/join" component={Join} />
           <Route exact path="/setting" component={UserSetting} />
-          <Route exact path="/Detail" component={Detail} />
+          <Route exact path="/detail" component={Detail} />
           <Route exact path="/discover" component={Discover} />
           <Route exact path="/start" component={Start} />
           <Route exact path="/editproject" component={EditProject} />
